d7c22e413f398afb124ff35c9d343e8f496e962e
Bipul <123935352+Syed-Bipul-Rahman@users.noreply.github.com>
2025-03-30 13:45:31 +0600

    Added page for video call

---
+++ lib/app/modules/loginPage/views/login_page_view.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_page_controller.dart';
+
+class LoginPageView extends GetView<LoginPageController> {
+  const LoginPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 16.h),
+              Text(
+                "Sign in to\nYour account",
+                style: GoogleFonts.outfit(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 24.sp,
+                ),
+              ),
+              Text(
+                "Welcome Back! Please enter your details.",
+                style: GoogleFonts.outfit(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14.sp,
+                ),
+              ),
+              SizedBox(height: 40.h),
+              Text(
+                "Your email",
+                style: GoogleFonts.outfit(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16.sp,
+                ),
+              ),
+              SizedBox(height: 4.h),
+              CustomInputField(
+                svgIconPath: AppImage.emailIcon,
+                hintText: 'Enter Email',
+                controller: TextEditingController(),
+              ),
+              SizedBox(height: 16.h),
+
+              Text(
+                "Password",
+                style: GoogleFonts.outfit(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16.sp,
+                ),
+              ),
+              SizedBox(height: 4.h),
+              CustomInputField(
+                svgIconPath: AppImage.lockIcon,
+                hintText: 'Enter password',
+                controller: TextEditingController(),
+                isPassword: true,
+              ),
+              SizedBox(height: 16.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Get.toNamed(Routes.FORGOT_PASSWORD);
+                    },
+                    child: Text(
+                      "Forgot Password?",
+                      style: GoogleFonts.outfit(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14.sp,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 32.h),
+              PrimaryButton(
+                onPressed: () {
+                  Get.toNamed(Routes.HOME);
+                },
+                text: "Sign In",
+                width: double.infinity,
+              ),
+              SizedBox(height: 16.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don’t have an account?",
+                    style: GoogleFonts.outfit(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.sp,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Get.toNamed(Routes.SIGN_UP_PAGE);
+                    },
+                    child: Text(
+                      "Sign up",
+                      style: GoogleFonts.outfit(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14.sp,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
